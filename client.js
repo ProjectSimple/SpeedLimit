ad5f76bf406d950ca68f033a419a602433397300
Reid Luna <reidluna@icloud.com>
2021-03-27 14:16:05 -0700

    1.1.0

--- client.js
+++ client.js
@@ -1,15 +1,25 @@
+let disable = true;
 setTick(() => {
-  if (IsPedInAnyVehicle(GetPlayerPed(-1), true))
+  if (IsPedInAnyVehicle(GetPlayerPed(-1), true)) {
+    disable = false;
+
+    let stName = GetStreetNameFromHashKey(
+      GetStreetNameAtCoord(...GetEntityCoords(GetPlayerPed(-1)))[0]
+    );
+    let ogLimit = FindByName(stName);
+
+    let limit = Math.round((ogLimit * 1.609) / 5) * 5;
+
+    if (!ogLimit) {
+      limit = "CD";
+      console.log("Contact Developer!", stName, ogLimit);
+    }
+
     SendNuiMessage(
       JSON.stringify({
-        disable: false,
-        limit:
-          FindByName(
-            GetStreetNameFromHashKey(
-              GetStreetNameAtCoord(...GetEntityCoords(GetPlayerPed(-1)))[0]
-            )
-          ) || "CD",
+        disable,
+        limit,
       })
     );
-  else SendNuiMessage(JSON.stringify({ disable: true }));
+  } else SendNuiMessage(JSON.stringify({ disable }));
 });
